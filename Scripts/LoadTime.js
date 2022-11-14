66a9ca667199dba1ec27dcf2466a7563ac18aa18
tatia2501 <catatat@yandex.ru>
2022-11-14 11:50:10 +0300

    fix: decoration changes

--- Scripts/LoadTime.js
+++ Scripts/LoadTime.js
@@ -1,11 +1,8 @@
 (() => {
     const startTime = new Date().getTime();
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const stampParagraph = document.querySelector('#timeinfo');
-
-        window.addEventListener('load', () => {
-            stampParagraph.innerHTML += `Время загрузки страницы - ${(new Date().getTime() - startTime) / 1000} секунд`;
-        });
-    })
+window.addEventListener('load', () => {
+    const infoParagraph = document.querySelector('#timeinfo');
+    infoParagraph.innerHTML += `Время загрузки страницы - ${(new Date().getTime() - startTime) / 1000} секунд`;
+});
 })();
